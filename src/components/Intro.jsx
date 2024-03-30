2422748573334e8974158b11ee264bd24aaac561
abiton198 <abitonp@gmail.com>
2024-03-30 20:50:22 +0200

    Intro page updated

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,33 +1,77 @@
 import React from 'react';
 
 const Intro = () => {
-  // Sample YouTube video ID for demonstration
-  const youtubeVideoId = "WodCbX6hhhE";
+  // Sample YouTube video IDs for demonstration
+  const youtubeVideoId1 = "WodCbX6hhhE";
+  const youtubeVideoId2 = 'VgxnjwesIAg';
+  // const youtubeVideoId3 = 'EFntprF5Kcw';
 
   return (
-    <div className='bg-white rounded-lg p-4 relative'>
-
-      {/* Responsive Image with YouTube Play Button */}
-      <div className='w-32 h-32 sm:w-48 sm:h-48 md:w-64 md:h-64 bg-gray-300  relative'>
-        {/* Overlay Image */}
-        <a href={`https://www.youtube.com/watch?v=${youtubeVideoId}`} target='_blank' rel='noopener noreferrer' className='absolute inset-0'>
-          <img
-            src={`https://img.youtube.com/vi/${youtubeVideoId}/0.jpg`} // Thumbnail URL
-            alt='YouTube Thumbnail'
-            className='w-full h-full object-cover rounded-full'
-          />
-        </a>
-        {/* YouTube Play Button */}
-        <a href={`https://www.youtube.com/watch?v=${youtubeVideoId}`} target='_blank' rel='noopener noreferrer' className='absolute inset-0 flex items-center justify-center'>
-          <svg className='w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 text-red-500 hover:text-red-700 cursor-pointer' fill='currentColor' viewBox='0 0 20 20'>
-            <path d='M10 0c5.523 0 10 4.477 10 10s-4.477 10-10 10S0 15.523 0 10 4.477 0 10 0zm4.429 10l-6.429-3.86v7.72L14.429 10z'></path>
-          </svg>
-        </a>
+    <div className="bg-white rounded-lg p-4 relative">
+      {/* Text on 1st video */}
+      <div className="mt-4">
+        <h2 className="text-black">The journey begins...</h2>
+        <p className="text-black">Are you ready for an exhilarating journey into the world of Duepoint business? In just 3 minutes, this video will whisk you away on an adventure that marks the beginning of an incredible financial journey. So grab a seat, buckle up, and let's embark on this thrilling ride together!</p>
       </div>
-      {/* YouTube Link */}
-      <div className='mt-4'>
-        <p className='text-black'>Business Intro</p>
-        <a href={`https://www.youtube.com/watch?v=${youtubeVideoId}`} target='_blank' rel='noopener noreferrer' className='text-blue-500'>Watch on YouTube</a>
+
+      {/* Video 1 */}
+      <div className="w-full md:flex justify-between items-center">
+        {/* Responsive Image with YouTube Play Button */}
+        <div className="w-full md:w-1/2 bg-gray-300 relative rounded-lg overflow-hidden">
+          {/* Overlay Image */}
+          <a href={`https://www.youtube.com/watch?v=${youtubeVideoId2}`} target="_blank" rel="noopener noreferrer" className="absolute inset-0">
+            <img
+              src={`https://img.youtube.com/vi/${youtubeVideoId2}/0.jpg`} // Thumbnail URL
+              alt="YouTube Thumbnail"
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </a>
+          {/* YouTube Play Button */}
+          <a href={`https://www.youtube.com/watch?v=${youtubeVideoId2}`} target="_blank" rel="noopener noreferrer" className="absolute inset-0 flex items-center justify-center">
+            <svg className="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 text-red-500 hover:text-red-700 cursor-pointer" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M10 0c5.523 0 10 4.477 10 10s-4.477 10-10 10S0 15.523 0 10 4.477 0 10 0zm4.429 10l-6.429-3.86v7.72L14.429 10z"></path>
+            </svg>
+          </a>
+        </div>
+
+        {/* YouTube Link */}
+        <div className="mt-4 md:mt-0 md:ml-4 w-full md:w-1/2">
+          <p className="text-black">Watch on YouTube</p>
+          <a href={`https://www.youtube.com/watch?v=${youtubeVideoId2}`} target="_blank" rel="noopener noreferrer" className="text-blue-500">Watch Now</a>
+        </div>
+      </div>
+
+      {/* Video 2 */}
+      <div className="mt-8">
+        <h2 className="text-black">The wealth exposed...</h2>
+        <p className="text-black">Embark on an extraordinary journey towards financial empowerment with Duepoint! This captivating video encapsulates the very essence of our revolutionary referral marketing business. As you delve into its compelling narrative, you'll discover the boundless opportunities that await. Join us today and become part of a dynamic community driven by success and prosperity. Let this video be your guiding light towards a brighter financial future with Duepoint!</p>
+      </div>
+
+      {/* Video 2 */}
+      <div className="w-full md:flex justify-between items-center mt-4">
+        {/* Responsive Image with YouTube Play Button */}
+        <div className="w-full md:w-1/2 bg-gray-300 relative rounded-lg overflow-hidden">
+          {/* Overlay Image */}
+          <a href={`https://www.youtube.com/watch?v=${youtubeVideoId1}`} target="_blank" rel="noopener noreferrer" className="absolute inset-0">
+            <img
+              src={`https://img.youtube.com/vi/${youtubeVideoId1}/0.jpg`} // Thumbnail URL
+              alt="YouTube Thumbnail"
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </a>
+          {/* YouTube Play Button */}
+          <a href={`https://www.youtube.com/watch?v=${youtubeVideoId1}`} target="_blank" rel="noopener noreferrer" className="absolute inset-0 flex items-center justify-center">
+            <svg className="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 text-red-500 hover:text-red-700 cursor-pointer" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M10 0c5.523 0 10 4.477 10 10s-4.477 10-10 10S0 15.523 0 10 4.477 0 10 0zm4.429 10l-6.429-3.86v7.72L14.429 10z"></path>
+            </svg>
+          </a>
+        </div>
+
+        {/* YouTube Link */}
+        <div className="mt-4 md:mt-0 md:ml-4 w-full md:w-1/2">
+          <p className="text-black">Business Intro</p>
+          <a href={`https://www.youtube.com/watch?v=${youtubeVideoId1}`} target="_blank" rel="noopener noreferrer" className="text-blue-500">Watch Now</a>
+        </div>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,70 @@
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+
+// const NavBar = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <nav className="bg-blue-900 py-4">
+//       <div className="container mx-auto px-4 md:px-0 flex justify-between items-center">
+//         <div className="text-white font-semibold text-lg md:text-xl">
+//           GenWealth <br />
+//           <p className='text-xs italic'>Creating a better tomorrow, today!</p>
+//         </div>
+//         <div className="md:hidden">
+//           <button
+//             onClick={toggleMenu}
+//             className="text-white hover:text-gray-100 focus:outline-none"
+//           >
+//             <svg
+//               className="w-6 h-6"
+//               fill="none"
+//               stroke="currentColor"
+//               viewBox="0 0 24 24"
+//               xmlns="http://www.w3.org/2000/svg"
+//             >
+//               {isOpen ? (
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   strokeWidth="2"
+//                   d="M6 18L18 6M6 6l12 12"
+//                 />
+//               ) : (
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   strokeWidth="2"
+//                   d="M4 6h16M4 12h16m-7 6h7"
+//                 />
+//               )}
+//             </svg>
+//           </button>
+//         </div>
+//         <div className={`md:flex md:items-center space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
+//           <Link to="/" className="text-white hover:text-gray-300 transition duration-300">Home</Link>
+//           <Link to="/About" className="text-white hover:text-gray-300 transition duration-300">About</Link>
+//           <Link to="/Intro" className="text-white hover:text-gray-300 transition duration-300">Intro</Link>
+//           <Link to="/Testimonies" className="text-white hover:text-gray-300 transition duration-300">Testimonies</Link>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default NavBar;
+
+
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../img/logo.jpeg';
 
-const NavBar = () => {
+export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -9,59 +72,61 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="bg-blue-900 py-4">
-      <div className="container mx-auto px-4 md:px-0 flex justify-between items-center">
-        <div className="text-white font-semibold text-lg md:text-xl">
-          GenWealth <br />
-          <p className='text-xs italic'>Creating a better tomorrow, today!</p>
-        </div>
-        <div className="md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-white hover:text-gray-100 focus:outline-none"
-          >
-            {isOpen ? (
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={logo} className="h-8" alt="GW" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GenWealth</span>
+        </Link>
+        <button
+          onClick={toggleMenu}
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-dropdown"
+          aria-expanded={isOpen ? "true" : "false"}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </button>
+        <div className={`w-full md:block md:w-auto ${isOpen ? 'block' : 'hidden'}`} id="navbar-dropdown">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link to="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</Link>
+            </li>
+            <li>
+              <button
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
-            )}
-          </button>
-        </div>
-        <div className={`md:flex space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
-          <Link to="/" className="text-white hover:text-gray-300 transition duration-300">Home</Link>
-          <Link to="/About" className="text-white hover:text-gray-300 transition duration-300">About</Link>
-          <Link to="/Intro" className="text-white hover:text-gray-300 transition duration-300">Intro</Link>
-          <Link to="/Testimonies" className="text-white hover:text-gray-300 transition duration-300">Testimonies</Link>
+                Dropdown 
+                <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+                </svg>
+              </button>
+              {/* <!-- Dropdown menu --> */}
+              <div id="dropdownNavbar" className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                  <li>
+                    <Link to="/" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/Intro" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Intro</Link>
+                  </li>
+                  <li>
+                    <Link to="/About" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">About</Link>
+                  </li>
+                </ul>
+                <div className="py-1">
+                  <Link to="/signout" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</Link>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
   );
-};
-
-export default NavBar;
+}
