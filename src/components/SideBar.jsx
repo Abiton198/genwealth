1b85c91ecadeb15b4947ee4e456281a04a5a5ea2
abiton198 <abitonp@gmail.com>
2024-03-31 17:23:53 +0200

    humburger updated

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,17 +7,21 @@ const Sidebar = () => {
   
   return (
     <div>
-      <nav className="bg-blue-900 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            {/* Assuming `logo` is defined */}
-            <img src={logo} className="h-8" alt="GW" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GenWealth</span>
-          </Link>
+     {/* <nav className="bg-blue-900 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 justify-self-end">
+            <div className="flex items-center space-x-3 rtl:space-x-reverse justify-between w-full">
+            
+            <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src={logo} className="h-8" alt="GW" />
+                <span className="justify-self-end text-2xl font-semibold whitespace-nowrap dark:text-white">GenWealth</span>
+            </Link>
+           
+            </div>
         </div>
-      </nav>
+        </nav> */}
 
-      <Menu>
+
+      <Menu width={'80%'} >
         <Link id="home" className="menu-item" to="/">Home</Link>
         <Link id="about" className="menu-item" to="/About">About</Link>
         <Link id="intro" className="menu-item" to="/Intro">Intro</Link>
